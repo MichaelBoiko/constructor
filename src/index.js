79c91bfc88e2e4f7b6c6307f5bb28e79090824f0
Michael Boiko <mchlboiko@gmail.com>
2024-04-15 22:17:56 +0300

    built project with classes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import {model} from './model';
-import {templates} from './templates';
+// import {templates} from './templates';
 import './styles/main.css';
 
 const site = document.querySelector('#site');
 model.forEach(el => {
-    const toHTML = templates[el.type];
-    if(toHTML){
-        site.insertAdjacentHTML('beforeend', toHTML(el));
-    }
+    // const toHTML = templates[el.type];
+    // if(toHTML){
+        site.insertAdjacentHTML('beforeend', el.toHTML());
+    // }
 });
--- src/model.js
+++ src/model.js
@@ -1,7 +1,8 @@
 import image from './assets/image.png';
+import {TitleBlock, ImageBlock, ColumnsBlock, TextBlock} from './classes/blocks';
 
 export const model = [
-    {type: 'title', value: 'Конструктор сайтов на JavaScript', options: {
+    new TitleBlock('Конструктор сайтов на JavaScript', {
         tag: 'h2',
         styles: {
             background: 'linear-gradient(#e66465, #9198e5)',
@@ -9,8 +10,8 @@ export const model = [
             padding: '1.5rem',
             color: '#fff',
         }
-    }},
-    {type: 'image', value: image, options: {
+    }),
+    new ImageBlock(image, {
         styles: {
             padding: '2rem 0',
             display: 'flex',
@@ -21,13 +22,13 @@ export const model = [
             height: 'auto',
             'border-radius': '50px',
         }
-    }},
-    {type: 'columns', value: [
+    }),
+    new ColumnsBlock([
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, consequuntur.',
         'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, deleniti vel!',
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum magnam nihil quam eum, eos ipsum.',
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, expedita. Laboriosam, iure.'
-    ], options: {
+    ], {
         styles: {
             background: 'linear-gradient(#e66465, #9198e5)',
             'text-align': 'center',
@@ -35,15 +36,15 @@ export const model = [
             color: '#fff',
             'font-weight': 'bold',
         }
-    }},
-    {type: 'text', value: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus 
+    }),
+    new TextBlock(`Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus 
     consequuntur deleniti, veritatis nihil officiis voluptas odit asperiores cum mollitia quam nemo 
-    reprehenderit cumque!`, options: {
+    reprehenderit cumque!`, {
         styles: {
             background: 'linear-gradient(#f2994a, #f2c94c)',
             padding: '1rem',
             'font-weight': 'bold',
             'text-align': 'center',
         }
-    }}
+    })
 ]
